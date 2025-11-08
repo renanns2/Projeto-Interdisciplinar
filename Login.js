930710fd518e0ac8bf840f7ebd4778a2deba445b
renanns2 <renannfsilva10@gmail.com>
2025-11-07 21:56:07 -0300

    Teste 2

--- Login.js
+++ Login.js
@@ -1,1 +1,9 @@
-const Registrar = document.getElementById("")
+const registrar = document.getElementById("registrar");
+const logar = document.getElementById("logar");
+const container = document.getElementById("container-barralateral")
+
+registrar.addEventListener("click", acionar());
+
+function acionar() {
+    registrar.classList.add('ativo');
+}
