c670cb817f055f70af9bd8c17e7d93beed1ac955
renanns2 <renannfsilva10@gmail.com>
2025-11-07 22:36:12 -0300

    A

--- Login.js
+++ Login.js
@@ -1,9 +1,9 @@
 const registrar = document.getElementById("registrar");
 const logar = document.getElementById("logar");
-const container = document.getElementById("container-barralateral")
+const co = document.getElementById("container")
 
-registrar.addEventListener("click", acionar());
+registrar.addEventListener("click", acionar);
 
 function acionar() {
-    container.classList.add('ativo');
+    container.classList.add('painel-direito-ativo');
 }
