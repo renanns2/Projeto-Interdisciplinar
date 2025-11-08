977824d69ed32b5835ba320fb4caa08eb5bd5df6
renanns2 <renannfsilva10@gmail.com>
2025-11-08 03:51:32 -0300

    Tela de Login Finalizada Front End
    Implementação de animações, e finalização do Front-End da tela de animação, apenas pequenos detalhes de responsividade e design faltam

--- Login/Login.js
+++ Login/Login.js
@@ -1,9 +1,30 @@
 const registrar = document.getElementById("registrar");
 const logar = document.getElementById("logar");
-const co = document.getElementById("container")
+const container = document.getElementById("container")
+const registroForm = document.querySelector(".formulario.registro");
+const logarForm = document.querySelector(".formulario.login");
 
-registrar.addEventListener("click", acionar);
+registrar.addEventListener("click", ativar);
 
-function acionar() {
+function ativar() {
     container.classList.add('painel-direito-ativo');
+
+    //Colocar timeout para mudar o zIndex, mudando apenas quando formulario e barralateral estão na mesma posição, no meio da pagina.
+    setTimeout(() => {
+        logarForm.style.zIndex = "1";
+        registroForm.style.zIndex = "5";
+    }, 300); 
+}       
+
+logar.addEventListener("click", desativar);
+
+function desativar() {
+
+    container.classList.remove('painel-direito-ativo');
+
+    setTimeout(() => {
+        logarForm.style.zIndex = "5";   
+        registroForm.style.zIndex = "1";
+    }, 300); 
+
 }
