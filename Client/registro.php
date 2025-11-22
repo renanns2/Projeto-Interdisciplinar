f8cf2b6843ed6cf37b3364f9eaba846546fc180c
renanns2 <renannfsilva10@gmail.com>
2025-11-21 21:13:44 -0300

    IMplementação do cabeçalho e mudanças na logica de login

--- Client/registro.php
+++ Client/registro.php
@@ -1,4 +1,33 @@
 <?php
     include_once "config.php";
-        
+
+    $email = $_POST['email'] ?? "";
+    $senha = $_POST['senha'] ?? "";
+    $nome = $_POST['nome'] ?? "";
+    $cargo = $_POST['cargo'] ?? "";
+
+
+    //Validação de dados
+    $erros = [];
+    if (empty($email)) {
+        $erros[] = 'Nenhum e-mail informado';
+    }
+    if (empty($senha)) {
+        $erros[] = 'Nenhuma senha informada';
+    }
+    if (empty($nome)) {
+        $erros[] = 'Nenhuma nome informada';
+    }
+    if (empty($cargo)) {
+        $erros[] = 'Nenhum cargo informado';
+    }
+    if (!empty($erros)) {
+        foreach($erros as $erro) {
+            echo "<p>$erro </p>";
+        }
+        exit;
+    }
+    
+    
+    
 ?>
